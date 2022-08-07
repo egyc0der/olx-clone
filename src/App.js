08c43408f787318492ba22932645ed1e1e08b55d
egyc0der <ahmed.pjt@hotmail.com>
2022-08-07 18:21:38 +0200

    adding heroku endpoints

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import styled from "@emotion/styled";
 import Navbar from "./components/Navbar";
